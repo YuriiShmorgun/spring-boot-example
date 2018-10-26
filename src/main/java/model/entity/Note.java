379c64c2c918096f532c42da11cb231563efd8a7
YuriiSH <yurashmorgun@gmail.com>
2018-10-26 15:59:23 +0300

    +index + @

--- src/main/java/model/entity/Note.java
+++ src/main/java/model/entity/Note.java
@@ -1,15 +1,13 @@
 package model.entity;
 
-
-
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDate;
-
 
 @Entity
 @Getter
@@ -19,12 +17,14 @@ public class Note {
     @Id
     @GeneratedValue
     private int id;
+    @Column (name = "message")
     private String message;
+    @Column (name = "date")
     private LocalDate date;
+    @Column (name = "done")
     private boolean done;
 
     public Note(int id) {
-
     }
 
     public Note(String message, LocalDate date, boolean done) {
